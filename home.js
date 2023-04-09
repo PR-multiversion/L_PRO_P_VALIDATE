b419c7ec14767971631266fbe76f044d51b2132f
PR-multiversion <96785476+PR-multiversion@users.noreply.github.com>
2023-04-09 22:08:53 +0530

    Delete home.js

--- home.js
+++
@@ -1,6 +0,0 @@
-let buybtn = document.querySelectorAll(".buy");
-buybtn.forEach(element => {
-    element.addEventListener("click", ()=>{
-        window.location.href = "/payment.html";
-    });
-});
